e1a02c3845bdeff7b68623afd231e43f4195a886
Vladyslav <kpigoodvlad@gmail.com>
2023-05-13 18:51:57 +0300

    And there was added a folder utils with setContent function

--- src/utils/setContent.js
+++ src/utils/setContent.js
@@ -6,15 +6,19 @@ const setContent = (process, Component, data) => {
     switch (process) {
         case 'waiting':
             return <Skeleton/>;
+            // eslint-disable-next-line
             break;
         case 'loading':
             return <Spinner/>;
+            // eslint-disable-next-line
             break;
         case 'confirmed':
             return <Component data={data}/>;
+            // eslint-disable-next-line
             break;
         case 'error':
             return <ErrorMessage/>;
+            // eslint-disable-next-line
             break;
         default:
             throw new Error('Unexpected process state');
